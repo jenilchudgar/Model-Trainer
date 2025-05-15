31cc94ac18043c838e61cb484242fc976f827426
Jenil Chudgar <jenilchudgarfamily@gmail.com>
2025-05-16 00:10:30 +0530

    Added Random Colors and fixed several bugs

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from tkinter import *
 from tkinter import filedialog,messagebox
 from PIL import ImageTk,Image
 from csv import DictWriter
-import io,os
+import io,os,random
 
 root = Tk()
 root.title("Image Editor")
@@ -15,10 +15,13 @@ o_path = ""
 current = 0
 
 start_x = start_y = 0
-rect_id = None
 color = "Red"
+rectangles = []
+rectangles_data = {}
 
 def show_image(index):
+    global rectangles
+    rectangles.clear()
     canvas.delete("all")
     canvas.image = images[index]  
     canvas.create_image(0, 0, image=canvas.image, anchor=NW)
@@ -56,50 +59,52 @@ def backward(e=None):
         forwardBtn.config(state=NORMAL)
 
 def save(e=None):
-    global rect_id,current,o_path,color
+    global current,o_path,color
     
     if not images:
         return
+    
+    if not rectangles:
+        messagebox.showerror("No Rectangle!","No rectangle has been drawn on the image. Kindly draw rectangle(s) and then click SAVE.")
+        return
 
-    if rect_id is not None:
-        c = canvas.coords(rect_id)
+    for rect_id in rectangles:
+        rect,clr = rect_id[0],rect_id[1]
+
+        c = canvas.coords(rect)
         cords_dict = {
-                "x1":c[0],
-                "y1":c[1],
-                "x2":c[2],
-                "y2":c[3],
-                "color":color,
-                "output":f"image{current+1}.png"
-            }
-        
-        field_names = ["x1","y1","x2","y2","color","output"]
-        
+            "x1":c[0],
+            "y1":c[1],
+            "x2":c[2],
+            "y2":c[3],
+            "color":clr,
+            "file":f"image{current+1}.png"
+        }
+
+        field_names = ["x1","y1","x2","y2","color","file"]
+                
         with open(f"{o_path}\\data.csv","a",newline="\n") as file:
             writer = DictWriter(file, fieldnames=field_names)
             writer.writerow(cords_dict)
 
-        show_image(current)
-        ps = canvas.postscript(colormode='color')
-        img = Image.open(io.BytesIO(ps.encode('utf-8')))
+    show_image(current)
+    ps = canvas.postscript(colormode='color')
+    img = Image.open(io.BytesIO(ps.encode('utf-8')))
 
-        if o_path:
-            img.save(f"{o_path}\\image{current+1}.png")
-        
-        messagebox.showinfo("Saved!","Your rectangle's co-ordinates have been saved in data.csv located in the output folder.")
-    else:
-        messagebox.showerror("No Rectangle!","No rectangle has been drawn on the image. Kindly draw a rectangle and then click SAVE.")
-        
+    if o_path:
+        img.save(f"{o_path}\\image{current+1}.png")
+               
 def mouse_press(event):
-    global start_x, start_y, rect_id
+    global start_x, start_y
     if switch_var.get() == "On":
         global color
         start_x, start_y = event.x, event.y
         rect_id = canvas.create_rectangle(start_x,start_y,start_x,start_y,outline=color.lower(),width=2)
+        rectangles.append((rect_id,color))
 
 def mouse_drag(event):
-    global rect_id
     if switch_var.get() == "On":
-        canvas.coords(rect_id,start_x,start_y,event.x,event.y)
+        canvas.coords(rectangles[-1][0],start_x,start_y,event.x,event.y)
 
 def switch_cmd(e):
     if not (switch_var.get() == "On" or switch_var.get() == "Off"):
@@ -179,14 +184,28 @@ def color_cmd(e=None):
     global color
     color = color_var.get()
 
+def random_clr():
+    global colors,color
+    clr = "#{:06x}".format(random.randint(0, 0xFFFFFF))
+    if clr not in colors:
+        colors.append(clr)
+        menu = color_switch['menu']
+        menu.delete(0,END)
+
+        for c in colors:
+            menu.add_command(label=c, command=lambda c=c: (color_var.set(c), color_cmd()))
+
+        color_var.set(clr)
+        color = color_var.get()
+
 statusBar = Label(root)
 canvas = Canvas(root)
 
-saveBtn = Button(root,text="Save",fg="red",command=save,font=("Calibri",20))
+saveBtn = Button(root,text="Save",fg="red",command=save,font=("Calibri",15))
 saveBtn.grid(row=1,column=1)
 
 forwardBtn = Button(root,text=">>",fg="blue",command=forward,font=("Calibri",25))
-forwardBtn.grid(row=1,column=2,pady=10)
+forwardBtn.grid(row=1,column=2)
 
 backwardsBtn = Button(root,text="<<",fg="blue",command=backward,state=DISABLED,font=("Calibri",25))
 backwardsBtn.grid(row=1,column=0)
@@ -195,15 +214,19 @@ clearBtn = Button(root,text="Clear Canvas",command=clear,font=("Calibri",15))
 clearBtn.grid(row=3,column=0)
 
 switch_frame = LabelFrame(root,text="Rectangle Tool")
-switch_frame.grid(row=3,column=1,padx=(0,15),pady=(10,20))
+switch_frame.grid(row=3,column=2,padx=(0,15),pady=(10,20),columnspan=2)
 
 switch_var = StringVar(value="On")
 switch = OptionMenu(switch_frame,switch_var,*["On","Off"],command=switch_cmd)
 switch.grid(row=0,column=0,pady=5,padx=10)
 
+colors = ["Red","Green","Blue","Yellow","Black"]
 color_var = StringVar(value="Red")
-color_switch = OptionMenu(switch_frame,color_var,*["Red","Green","Blue","Yellow","Black"],command=color_cmd)
+color_switch = OptionMenu(switch_frame,color_var,*colors)
 color_switch.grid(row=0,column=1,pady=5,padx=10)
+
+random_btn = Button(switch_frame,text="Random Color",command=random_clr)
+random_btn.grid(row=0,column=2,pady=5,padx=10)
 
 root.bind("<Left>",backward)
 root.bind("<Right>",forward)
